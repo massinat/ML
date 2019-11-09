81cb9e8eafc2622c1d23d6be589aac7dd469a185
massinat84 <massinat84@hotmail.it>
2019-11-09 16:56:23 +0100

    Plot updated

--- part1.py
+++ part1.py
@@ -102,14 +102,24 @@ if __name__=="__main__":
             x.append(int(row[0]))
             yCorrect.append(float(row[1]))
             yWrong.append(float(row[2]))
-            yAccuracy.append('%.2f'%(float(row[1]) * 100 / float(row[0])))
+            yAccuracy.append(float(row[1]) * 100 / float(row[0]))
     
+    plt.subplot(2, 1, 1)
     plt.plot(x, yCorrect)
     plt.plot(x, yWrong)
+    plt.annotate(int(yCorrect[-1]), xy=(x[-1] + 3, yCorrect[-1]))
+    plt.annotate(int(yWrong[-1]), xy=(x[-1] + 3, yWrong[-1]))
     plt.xlabel("Total instances")
     plt.ylabel("Classified instances")
     plt.title("Model behaviour K=1")
-    plt.legend(["y = Correct", "y = wrong"], loc="upper left")
+    plt.legend(["y = Correct", "y = wrong"], loc="upper left")    
+
+    plt.subplot(2, 1, 2)
+    plt.plot(x, yAccuracy)
+    plt.annotate(f"{yAccuracy[-1]}%", xy=(x[-1] + 3, yAccuracy[-1]))
+    plt.xlabel("Total instances")
+    plt.ylabel("% Accuracy")
+
     plt.show()
 
 
